634d7768a611f525380ad607a9b9f71b7abc83de
Matt McInerney <matt@township.agency>
2022-05-07 12:23:14 -0400

    started

---
+++ names.js
@@ -0,0 +1,32 @@
+var locations = [
+
+  "Key of C",
+
+  "Key of F",
+
+  "Key of B Flat",
+
+  "Key of E Flat",
+
+"Key of A Flat",
+
+  "Key of D Flat",
+
+"Key of G Flat",
+
+"Key of C Flat",
+
+"Key of G",
+
+  "Key of D",
+
+"Key of A",
+
+"Key of E",
+
+"Key of B",
+
+  "Key of F Sharp",
+
+  "Key of C Sharp",
+];
