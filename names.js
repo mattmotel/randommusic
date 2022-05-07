909e5d60514570b84226e443654e794a86b516ba
Matt McInerney <matt@township.agency>
2022-05-07 15:47:02 -0400

    add og

--- names.js
+++ names.js
@@ -1,33 +1,18 @@
-var locations = [
-
+var keys = [
   "Key of C",
-
   "Key of F",
-
   "Key of B Flat",
-
   "Key of E Flat",
-
   "Key of A Flat",
-
   "Key of D Flat",
-
   "Key of G Flat",
-
   "Key of C Flat",
-
   "Key of G",
-
   "Key of D",
-
   "Key of A",
-
   "Key of E",
-
   "Key of B",
-
   "Key of F Sharp",
-
   "Key of C Sharp",
 ];
 var scale = [
